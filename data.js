67b2e9ff4d0905e01796dc59fdc749d03ee7dd43
Midokhalil1 <midokhalil22@gmail.com>
2022-10-17 17:09:32 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,13 @@
+export default [{
+"name": "Brad Jones",
+"age":30,
+"birthday": "oct, 15, 1992"
+},{
+    "name": "Todd Albert",
+    "age": 47,
+    "birthday": "oct 17, 1975"
+},{
+    "name":"Mido Khalil",
+    "age":37,
+    "birthday":"oct 17,1985"
+}]
